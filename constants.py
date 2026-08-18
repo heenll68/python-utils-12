a5f3571f7bb3a530b4520146f0cb191e067d2c78
Katarzyna Nowak <j3571603@gmail.com>
2026-08-18 18:14:26 +0200

    fix:optimized constants for performance

--- constants.py
+++ constants.py
@@ -1,27 +1,30 @@
-AUTOCLICKER_SPEED = 0.1  # Time in seconds between clicks
-AUTOCLICKER_DEFAULT_COUNT = 100  # Default number of clicks
-AUTOCLICKER_MAX_CLICKS = 10000  # Maximum allowed clicks
-CLICK_BUTTON = "left"  # Default click button (left/right)
-AUTOCLICKER_MODE = "single"  # Modes: single or continuous
-
-# Define possible click modes
-CLICK_MODES = {
-    "single": "Single click once",
-    "continuous": "Hold down for continuous clicking"
-}
-
-# Hotkey settings
-HOTKEYS = {
-    "start_stop": "ctrl+shift+s",  # Hotkey to start/stop clicking
-    "exit": "ctrl+shift+e"  # Hotkey to exit the application
-}
+MAX_CLICKS = 1000
+MIN_CLICKS = 1
+DEFAULT_DELAY = 0.1
+CLICK_BUTTON = 'left'
+CLICK_PATTERN = {'type': CLICK_BUTTON, 'count': MAX_CLICKS}
 
 # Error messages
-ERROR_MESSAGES = {
-    "invalid_mode": "Invalid click mode selected.",
-    "input_out_of_bounds": "Input value is out of allowed range."
+ERROR_NO_CLICKS = 'At least one click must be specified'
+ERROR_DELAY_TOO_SHORT = 'Delay must be greater than zero'
+
+# Settings
+DEFAULT_SETTINGS = {
+    'clicks': 100,
+    'delay': DEFAULT_DELAY,
+    'button': CLICK_BUTTON
 }
 
-# General settings
-DEBUG_MODE = False  # Enable or disable debug information
-LOG_FILE = "autoclicker.log"  # Log file location
+# Configuration bounds
+DELAY_BOUNDS = (0.01, 5.0)
+
+# Operating system
+IS_WINDOWS = True if os.name == 'nt' else False
+IS_MAC = True if os.uname().sysname == 'Darwin' else False
+IS_LINUX = True if os.name == 'posix' and not IS_MAC else False
+
+# Click pattern
+CLICK_PATTERN = {
+    'button': CLICK_BUTTON,
+    'delay_range': DELAY_BOUNDS
+}
